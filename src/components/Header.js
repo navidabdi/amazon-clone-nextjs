eeb72ba4df7d073e764223a7a8b1872c98a38d5e
navidabdi <artsanaat@gmail.com>
2022-03-07 18:36:20 +0330

    done with the top nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,8 +18,31 @@ const Header = () => {
             className="cursor-pointer"
           />
         </div>
-        <div>
-          <input type="text" />
+        {/* Search */}
+        <div className="hidden sm:flex items-center h-10 rounded-md bg-yellow-400 hover:bg-yellow-500 transition duration-150 flex-grow cursor-pointer">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+        {/* Right */}
+        <div className="text-white flex items-center justify-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p className="">Hello Nabi Abdi</p>
+            <p className="font-bold md:text-sm">Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-bold md:text-sm">& Orders</p>
+          </div>
+          <div className="link relative flex items-center">
+            <span className="absolute bg-yellow-400 top-0 text-center text-black font-bold right-0 h-4 w-4 rounded-full md:right-10">
+              0
+            </span>
+            <ShoppingCartIcon className="h-8" />
+            <p className="hidden md:inline font-bold md:text-sm mt-2">Basket</p>
+          </div>
         </div>
       </div>
       {/* Bottom Nav */}
